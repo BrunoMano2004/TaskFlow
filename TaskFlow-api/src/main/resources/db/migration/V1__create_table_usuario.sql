6ca354e3faddc172d49b5bcfc09c062946f70eb8
Bruno Mano <143518260+BrunoMano2004@users.noreply.github.com>
2024-09-12 15:47:09 -0300

    Removendo pasta para adicionar algumas atualizações

--- TaskFlow-api/src/main/resources/db/migration/V1__create_table_usuario.sql
+++
@@ -1,9 +0,0 @@
-create table usuario (
-    id bigint not null primary key auto_increment,
-    email varchar(100) unique not null,
-    nome_completo varchar(100) not null,
-    data_nascimento date not null,
-    tema_escuro bool default false,
-    data_cadastro timestamp default current_timestamp,
-    img_perfil blob
-)
